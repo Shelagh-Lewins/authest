5e65c460ba5da911ab44105f597ff60bf8f26059
Shelagh Lewins <shelagh_lewins@hotmail.com>
2018-12-28 17:20:08 +0000

    Work on bootstrap navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,51 +8,72 @@ import { logoutUser } from '../actions/authentication';
 import { withRouter } from 'react-router-dom';
 
 class Navbar extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      'showDropdown': false,
+    };
+  }
+  showDropdown(e) {
+    e.preventDefault();
+    this.setState(prevState => ({
+      'showDropdown': !prevState.showDropdown,
+    }));
+  }
 
-    onLogout(e) {
-        e.preventDefault();
-        this.props.logoutUser(this.props.history);
-    }
+	onLogout(e) {
+		e.preventDefault();
+		this.props.logoutUser(this.props.history);
+	}
 
-    render() {
-        const {isAuthenticated, user} = this.props.auth;
-        const authLinks = (
-            <ul className="navbar-nav ml-auto">
-                <a href="#" className="nav-link" onClick={this.onLogout.bind(this)}>
-                    <img src={user.avatar} alt={user.name} title={user.name}
-                        className="rounded-circle"
-                        style={{ width: '25px', marginRight: '5px'}} />
-                            Logout
-                </a>
-            </ul>
-        )
-      const guestLinks = (
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-                <Link className="nav-link" to="/register">Sign Up</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/login">Sign In</Link>
-            </li>
-        </ul>
-      )
-        return(
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/">Redux Node Auth</Link>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    {isAuthenticated ? authLinks : guestLinks}
-                </div>
-            </nav>
-        )
-    }
+	render() {
+		const {isAuthenticated, user} = this.props.auth;
+		const authLinks = (
+			<ul className="navbar-nav ml-auto">
+				<a href="/" className="nav-link" onClick={this.onLogout.bind(this)}>
+					<img src={user.avatar} alt={user.name} title={user.name}
+						className="rounded-circle"
+						style={{ width: '25px', marginRight: '5px'}} />
+							Logout
+				</a>
+			</ul>
+		)
+	  const guestLinks = (
+		<ul className="navbar-nav ml-auto">
+			<li className="nav-item">
+				<Link className="nav-link" to="/register">Sign Up</Link>
+			</li>
+			<li className="nav-item">
+				<Link className="nav-link" to="/login">Sign In</Link>
+			</li>
+		</ul>
+	  )
+		return(
+			<nav className="navbar navbar-expand-lg navbar-light bg-light">
+				<Link className="navbar-brand" to="/">Redux Node Auth</Link>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+					onClick={(e) => {this.showDropdown(e)}} >
+		  		<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className={`collapse navbar-collapse ${this.state.showDropdown ? 'show' : ''}`} id="navbarSupportedContent">
+					{isAuthenticated ? authLinks : guestLinks}
+					<form className="form-inline my-2 my-lg-0">
+						<input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+						<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				  </form>
+				</div>
+				
+			</nav>
+		)
+	}
 }
 Navbar.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
+	logoutUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-    auth: state.auth
+	auth: state.auth
 })
 
 export default connect(mapStateToProps, { logoutUser })(withRouter(Navbar));
